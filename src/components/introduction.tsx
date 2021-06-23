8c23188371946c4a5d1947d415a8ef68b0af13eb
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-06-23 11:56:02 +0100

    created introduction component

---
+++ src/components/introduction.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Introduction = () => {
+    return (
+        <h1>Welcome to Clevercut</h1>
+    )
+}
+
+export default Introduction;
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Introduction from "../components/introduction"
+
+const Home = () => {
+    return (<>
+        <Introduction />
+    </>)
+}
+
+export default Introduction
